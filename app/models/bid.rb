735d5812a247520d13403663eabf43d70dae52e4
Phil Greene <pgreeneiii@users.noreply.github.com>
2017-12-03 20:30:48 -0600

    Continuing to work on Section View

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,6 @@
 class Bid < ApplicationRecord
    belongs_to(:section, :class_name => "Section", :foreign_key => "section_id")
+   belongs_to(:timetable, :class_name => "TimeTable", :foreign_key => "time_id")
+   belongs_to(:daytable, :class_name => "DayTable", :foreign_key => "day_id")
+   belongs_to(:quartertable, :class_name => "QuarterTable", :foreign_key => "quarter_id")
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,6 +2,7 @@ class Schedule < ApplicationRecord
    belongs_to(:section, :class_name => "Section", :foreign_key => "section_id")
    belongs_to(:timetable, :class_name => "TimeTable", :foreign_key => "time")
    belongs_to(:daytable, :class_name => "DayTable", :foreign_key => "day")
+   belongs_to(:quartertable, :class_name => "QuarterTable", :foreign_key => "quarter")
    has_one(:course, :through => :section)
    has_one(:professor, :through => :section)
 
